0f455411ed4bdecba958c277456a05b5322529b3
JohnReiland <reiland.shanghai@gmail.com>
2019-11-12 14:11:43 -0500

    "Complete readAll, readOne, and update refactorization"

--- datastore/index.js
+++ datastore/index.js
@@ -25,62 +25,73 @@ exports.create = (text, callback) => {
 };
 
 exports.readAll = (callback) => {
-  // get an array of objects with id and text field?
-  // we can fill in text with id not real text for now.
   let err = null;
   let data = [];
-
-  // console.log('data array, pre-readDir: ' + JSON.stringify(data));
-
-  // for (let id in items) { //for.. in is not the way to do this; instead, read directory contents and loop over them
-
-  console.log('dataDir: ' + exports.dataDir);
   fs.readdir(exports.dataDir, (err, files) => {
-    if (err) {
-      throw err;
-    }
-    if (files.length > 0) {
-      console.log('files: ' + files + ' files is array?: ' + (Array.isArray(files)) + ' element is of type: ' + (typeof files[0]));
-    }
     for (var i = 0; i < files.length; i++) {
       let split = files[i].split(".");
       let el = split[0]
-      console.log('el: ' + el)
       data.push({
         id: el,
         text: el
       })
-      console.log('data array, mid-readDir: ' + JSON.stringify(data));
     }
+    callback(err, data);
   });
-  console.log('data array, post-readDir: ' + JSON.stringify(data));
-  // let text = id;
-    //let todoPath = path.join(exports.dataDir, id + '.txt');
-    //text = fs.readFile(todoPath); // this will be done later.
-  // let obj = { id, text };
-  // var data = _.map(items, (text, id) => {
-  //   return { id, text };
-  // });
-  callback(err, data); //callback(null, data) //originally
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  //get file path
+  //then call read file
+  let todoPath = path.join(exports.dataDir, id + '.txt');
+  fs.readFile(todoPath, (err, data) => {
+    if (data) {
+      callback(err, {id, text: data.toString()});
+    } else {
+      callback(err, {id, text: null});
+    }
+  });
+
+  // var text = items[id];
+  // if (!text) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback(null, { id, text });
+  // }
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  //just write to todoPath with text then callback within that
+  let todoPath = path.join(exports.dataDir, id + '.txt');
+  let data = [];
+  fs.readdir(exports.dataDir, (err, files) => {
+    for (var i = 0; i < files.length; i++) {
+      let split = files[i].split(".");
+      let elt = split[0];
+      data.push(elt);
+    }
+    if(data.includes(id)) {
+      fs.writeFile(todoPath, text, (err) => {
+      callback(err, {id, text});
+      })
+    } else {
+      callback(new Error("update error"), null);
+    };
+  });
+
+
+
+
+
+
+
+  // var item = items[id];
+  // if (!item) {
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   items[id] = text;
+  //   callback(null, { id, text });
+  // }
 };
 
 exports.delete = (id, callback) => {
